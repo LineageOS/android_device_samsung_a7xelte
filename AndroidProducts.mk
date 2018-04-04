47430a6661fc9756247d607688efadb9c6abeb8e
Dan Willemsen <dwillemsen@google.com>
2018-04-04 16:49:07 -0700

    a7xelte: Switch from add_lunch_combo to COMMON_LUNCH_CHOICES
    Bug: 77599627
    Test: lunch  [see entries in list]
    Change-Id: Id44fcfec47eeb979a745f879031f1bec7359a633
    
    # Conflicts:
    #	AndroidProducts.mk
    #	vendorsetup.sh

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -18,3 +18,8 @@ LOCAL_PATH := device/samsung/a7xelte
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_PATH)/lineage_a7xelte.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_a7xelte-user \
+    lineage_a7xelte-userdebug \
+    lineage_a7xelte-eng
