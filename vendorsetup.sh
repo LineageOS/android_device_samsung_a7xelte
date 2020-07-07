sourajitk/device_samsung_a7xelte

a7xelte: Add vendorsetup.sh
* This enables bash auto-complete for lunch choices.

Change-Id: I706df4cb87943c5cfd2b9dbd90f702f0675f62ca

vendorsetup.sh
@@ -1,0 +1,2 @@
+add_lunch_combo lineage_a5xelte-eng
+add_lunch_combo lineage_a5xelte-userdebug

LineageOS/android_device_samsung_a7xelte

vendorsetup.sh
@@ -1,0 +1,2 @@
+add_lunch_combo lineage_a7xelte-eng
+add_lunch_combo lineage_a5xelte-userdebug