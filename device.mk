sourajitk/device_samsung_a7xelte

a7xelte: Inherit proprietary vendor files
Change-Id: Ibb0d799562e6cc82f85f3d5f9cfb726f26fc7757

device.mk
@@ -69,3 +69,5 @@ PRODUCT_COPY_FILES += \
 # Inherit from universal7580-common
 $(call inherit-product, device/samsung/universal7580-common/device-common.mk)
 
+# Call the proprietary setup
+$(call inherit-product, vendor/samsung/a5xelte/a5xelte-vendor.mk)

LineageOS/android_device_samsung_a7xelte

device.mk
@@ -69,3 +69,5 @@ PRODUCT_COPY_FILES += \
 # Inherit from universal7580-common
 $(call inherit-product, device/samsung/universal7580-common/device-common.mk)
 
+# Call the proprietary setup
+$(call inherit-product, vendor/samsung/a7xelte/a7xelte-vendor.mk)