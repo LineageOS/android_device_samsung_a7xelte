sourajitk/device_samsung_a7xelte

a7xelte: Set kernel defconfig
Change-Id: I9417226a048a35cc62cc010cb5a75cde9740ab74

BoardConfig.mk
@@ -37,3 +37,6 @@ BOARD_RECOVERYIMAGE_PARTITION_SIZE := 35651584
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 3145728000
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 13514047488
 BOARD_FLASH_BLOCK_SIZE := 4096
+
+# Kernel
+TARGET_KERNEL_CONFIG := lineageos_a5xelte_defconfig

LineageOS/android_device_samsung_a7xelte

BoardConfig.mk
@@ -37,3 +37,6 @@ BOARD_RECOVERYIMAGE_PARTITION_SIZE := 35651584
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 3145728000
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 13514047488
 BOARD_FLASH_BLOCK_SIZE := 4096
+
+# Kernel
+TARGET_KERNEL_CONFIG := lineageos_a7xelte_defconfig